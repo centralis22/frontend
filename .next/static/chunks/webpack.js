692e012d7f80c2deb5fa05af59f822ea6b55f7ec
William Hu <willyhu12333@gmail.com>
2022-10-03 10:42:27 -0700

    Modified .gitignore file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "82e449a8badc01dd"; }
+/******/ 		__webpack_require__.h = function() { return "aa336c6708abba78"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
