0d2fee17d13a733b6effa695b3252b5240e12d77
William Hu <willyhu12333@gmail.com>
2022-10-23 20:54:11 -0700

    Changed student Dashboard

--- components/StudentLayout.jsx
+++ components/StudentLayout.jsx
@@ -1,11 +1,9 @@
-import StudentDashboard from './StudentDashboard';
 import Banner from "./Banner";
 
 function StudentLayout({ children, sessionID, CurrentPage}) {
   return (
     <div>
       <Banner UserType="Student" sessionID={sessionID} Session={true}/>
-      <StudentDashboard CurrentPage={CurrentPage}/>
       <div className="main">{children}</div>
     </div>
   );
