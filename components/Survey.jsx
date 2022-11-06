e4189a596553eba2a5dbde2876454db95a09fd92
William Hu <willyhu12333@gmail.com>
2022-11-06 14:13:25 -0800

    Formated Survey Page

--- components/Survey.jsx
+++ components/Survey.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useRouter } from "next/router";
 import sock from "../config/socket";
 
-function Survey({surveyNumber, user}) {
+function Survey({ surveyNumber, user }) {
   var router = useRouter();
-  var isStudent = {user} === "Student";
+  var isStudent = user === "Student";
 
   /**
    * Survey answers.
@@ -57,134 +57,155 @@ function Survey({surveyNumber, user}) {
   }
 
   return (
-    <div>
-      <p className="surveyTitle">Survey {surveyNumber}</p>
-      <div>
-        <span className="surveyQuestion">
-          This round, did your team decide to change anything regarding its
-          product strategy?
-        </span>
-        <div className="surveyChoices">
-          <input
-            type="radio"
-            onChange={saveText}
-            value="No change was made and the team decided to stay on course with its
-          plan"
-            name="question1"
-          />{" "}
-          No change was made and the team decided to stay on course with its
-          plan
-          <br />
-          <input
-            type="radio"
-            onChange={saveText}
-            value="Some changes were made by modifying one or two points and then keeping
-          the remaining elements the same"
-            name="question1"
-          />{" "}
-          Some changes were made by modifying one or two points and then keeping
-          the remaining elements the same
-          <br />
-          <input
-            type="radio"
-            onChange={saveText}
-            value="Major changes were made in taking the original plan into a new
-          direction"
-            name="question1"
-          />{" "}
-          Major changes were made in taking the original plan into a new
-          direction
-          <br />
-          <br />
+    <div className="pageSurvey">
+      <div className="surveyBox">
+        <p className="surveyTitle">Survey {surveyNumber}</p>
+        <div className="surveyWarning">
+          <span>
+            Warning: *You can only submit your responses once. Please make sure
+            you are ready to submit your answers before clicking the submit
+            button.
+          </span>
         </div>
-      </div>
-      <div>
-        <span className="surveyQuestion">
-          If you made a change in product strategy, please briefly describe it:
-        </span>
         <div>
-          <textarea
-            className="questionTextField"
-            onChange={saveText}
-            name="question2"
-            width="500px"
-            value={answer.question2}
-            rows="3"
-          />
+          <span className="surveyQuestion">
+            This round, did your team decide to change anything regarding its
+            product strategy?
+          </span>
+          <div className="surveyChoices">
+            <input
+              type="radio"
+              onChange={saveText}
+              value="No change was made and the team decided to stay on course with its
+          plan"
+              name="question1"
+            />{" "}
+            No change was made and the team decided to stay on course with its
+            plan
+            <br />
+            <input
+              type="radio"
+              onChange={saveText}
+              value="Some changes were made by modifying one or two points and then keeping
+          the remaining elements the same"
+              name="question1"
+            />{" "}
+            Some changes were made by modifying one or two points and then
+            keeping the remaining elements the same
+            <br />
+            <input
+              type="radio"
+              onChange={saveText}
+              value="Major changes were made in taking the original plan into a new
+          direction"
+              name="question1"
+            />{" "}
+            Major changes were made in taking the original plan into a new
+            direction
+            <br />
+            <br />
+          </div>
         </div>
-        <br />
-      </div>
-      <div>
-        <span className="surveyQuestion">
-          This round, did your team decide to pursue the potential new investor?
-        </span>
-        <div className="surveyChoices">
-          <input type="radio" onChange={saveText} value="No" name="question3" />{" "}
-          No
-          <br />
-          <input
-            type="radio"
-            onChange={saveText}
-            value="Hesitantly"
-            name="question3"
-          />{" "}
-          Hesitantly
-          <br />
-          <input
-            type="radio"
-            onChange={saveText}
-            value="Yes"
-            name="question3"
-          />{" "}
-          Yes
-          <br />
-          <br />
-        </div>
-      </div>
-      <div>
-        <span className="surveyQuestion">
-          This round, did your team decide to change anything regarding its
-          product strategy?
-        </span>
-        <br />
-        <div className="surveyChoices">
-          <input type="radio" onChange={saveText} value="No" name="question4" />{" "}
-          No
-          <br />
-          <input
-            type="radio"
-            onChange={saveText}
-            value="Yes - add someone"
-            name="question4"
-          />{" "}
-          Yes - add someone
-          <br />
-          <input
-            type="radio"
-            onChange={saveText}
-            value="Yes - remove someone or reduce their role"
-            name="question4"
-          />{" "}
-          Yes - remove someone or reduce their role
-          <br />
+        <div>
+          <span className="surveyQuestion">
+            If you made a change in product strategy, please briefly describe
+            it:
+          </span>
+          <div>
+            <textarea
+              className="questionTextField"
+              onChange={saveText}
+              name="question2"
+              width="500px"
+              value={answer.question2}
+              rows="3"
+            />
+          </div>
           <br />
         </div>
-      </div>
-      <div>
-        <span className="surveyQuestion">
-          If you made a change in the people on the team, please briefly
-          describe it:
-        </span>
-        <br />
-        <div className="surveyChoices">
-          <textarea
-            className="questionTextField"
-            onChange={saveText}
-            name="question5"
-            width="500px"
-            value={answer.question5}
-            rows="3"
-          />
+        <div>
+          <span className="surveyQuestion">
+            This round, did your team decide to pursue the potential new
+            investor?
+          </span>
+          <div className="surveyChoices">
+            <input
+              type="radio"
+              onChange={saveText}
+              value="No"
+              name="question3"
+            />{" "}
+            No
+            <br />
+            <input
+              type="radio"
+              onChange={saveText}
+              value="Hesitantly"
+              name="question3"
+            />{" "}
+            Hesitantly
+            <br />
+            <input
+              type="radio"
+              onChange={saveText}
+              value="Yes"
+              name="question3"
+            />{" "}
+            Yes
+            <br />
+            <br />
+          </div>
+        </div>
+        <div>
+          <span className="surveyQuestion">
+            This round, did your team decide to change anything regarding its
+            product strategy?
+          </span>
+          <br />
+          <div className="surveyChoices">
+            <input
+              type="radio"
+              onChange={saveText}
+              value="No"
+              name="question4"
+            />{" "}
+            No
+            <br />
+            <input
+              type="radio"
+              onChange={saveText}
+              value="Yes - add someone"
+              name="question4"
+            />{" "}
+            Yes - add someone
+            <br />
+            <input
+              type="radio"
+              onChange={saveText}
+              value="Yes - remove someone or reduce their role"
+              name="question4"
+            />{" "}
+            Yes - remove someone or reduce their role
+            <br />
+            <br />
+          </div>
+        </div>
+        <div>
+          <span className="surveyQuestion">
+            If you made a change in the people on the team, please briefly
+            describe it:
+          </span>
+          <br />
+          <div>
+            <textarea
+              className="questionTextField"
+              onChange={saveText}
+              name="question5"
+              width="500px"
+              value={answer.question5}
+              rows="3"
+            />
+          </div>
         </div>
       </div>
       {isStudent ? (
