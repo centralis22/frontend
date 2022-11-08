f5aa4558d4f44cb1bbb752842871967cd3568c5d
William Hu <willyhu12333@gmail.com>
2022-11-08 02:33:47 -0800

    test1

--- pages/session-results.js
+++ pages/session-results.js
@@ -10,23 +10,12 @@ export default function SessionResults() {
   const router = useRouter();
 
   const { sessionID } = useUserContext();
-  var filename = toString(sessionID);
 
   sock.onmessage = function (e) {
     var parsedData = JSON.parse(e.data);
 
     if (parsedData.respond_id === 4888 && parsedData.status_code === 200) {
-      fetch("http://localhost:8080/downloadFile/534639").then((response) => {
-        response.blob().then((blob) => {
-          let url = window.URL.createObjectURL(blob);
-          let a = document.createElement("a");
-          a.href = url;
-          a.download = "534639.zip";
-          a.click();
-        });
-        
-        window.location.href = response.url;
-      });
+      window.location.href = 'http://localhost:8080/downloadFile/534639';
     }
   };
 
